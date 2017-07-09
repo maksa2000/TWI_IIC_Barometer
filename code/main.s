8024ed5e397a222d4b27b1e86efc7c7a95c2064f
maksa2000 <maksa2k@gmail.com>
2017-07-09 18:06:55 +0300

    Changes 20170709
    First time recieved data from BMP085 (one of EEPROM stoed calibrations)

--- code/main.s
+++ code/main.s
@@ -1,6 +1,7 @@
 ; interrupt vector table
 ; NB! Better allways create whole vector table
-.text 0
+
+;.text 0
 .org 0x0000
 jmp main									; reset vector
 jmp return_from_interrupt					; 0x0002
@@ -26,10 +27,70 @@ jmp return_from_interrupt					; 0x0028
 jmp return_from_interrupt					; 0x002A
 jmp return_from_interrupt					; 0x002C
 jmp return_from_interrupt					; 0x002E
-jmp bmp_085_twi_interrupt					; 0x0030
+jmp return_from_interrupt					; 0x0030
 jmp return_from_interrupt					; 0x0032
 
-; function includes should be allways after interrupt vector table
+;.balign 2	; this will align main function to even byte (otherwise code is not executable)
+main:
+	; set debuging led for blinking
+	call init_led
+	
+	; enable sleep mode (idle)
+	ldi r24, 0x01				
+	call set_sleep_mode
+	
+	; init usart
+	; USART for some reason generates TWI interrupt, so I disable it
+	call usart_init_rx_tx
+	call usart_disable_interupts
+	
+	; initialize interfaces and internal variables for bmp085 
+	call bmp_085_init
+	
+	;call watchdog_init_interrupt_mode							
+	
+	sei
+	
+	;call twi_send_start_condition
+	
+	ldi r24, BMP085_AC1_LSB
+	ldi r25, BMP085_AC1_MSB
+	call bmp_085_read_calibrations
+	
+_sleep_loop:	
+	sleep
+	;call bmp_085_reset_actions_and_states
+	rjmp _sleep_loop
+	ret
+	
+; send twi status to usrt
+send_to_usart:
+	push r24
+	push r25
+	call hex2str
+	call usart_transmit_data
+	mov r24, r25
+	call usart_transmit_data
+	pop r25
+	pop r24
+	ret
+	
+delayFunc:
+    ldi  r18, 41
+    ldi  r19, 150
+    ldi  r20, 128
+L1: dec  r20
+    brne L1
+    dec  r19
+    brne L1
+    dec  r18
+    brne L1
+	ret
+	
+return_from_interrupt:
+	reti
+	
+; function includes should be allways after interrupt vector table and better after main function
 .ifndef ATMEGA328P_CONSTANTS
 .include "atmega328p_core/const_codes.inc"
 .endif
@@ -49,7 +110,7 @@ jmp return_from_interrupt					; 0x0032
 .ifndef ARDUINO_LED
 .include "arduino_core/led.inc"
 .endif
-
+	
 .ifndef BMP_085_MODULE
 .include "bmp_085_module/bmp_085.inc"
 .endif
@@ -57,57 +118,5 @@ jmp return_from_interrupt					; 0x0032
 .ifndef WATCHDOG
 .include "atmega328p_core/watchdog.inc"
 .endif
-
-.equ READ_CALIBRATION_ACTION, 0x01
-.equ READ_DATA_ACTION, 0x02
-.equ WRITE_DATA_ACTION, 0x03
-
-.balign 2	; this will align main function to even byte (otherwise code is not executable)
-main:
-	; set debuging led for blinking
-	call init_led
-	
-	; enable sleep mode (idle)
-	ldi r24, 0x01				
-	call set_sleep_mode
-	
-	; init usart
-	; USART for some reason generates TWI interrupt, so I disable it
-	call usart_init_rx_tx
-	call usart_disable_interupts
-	
-	; initialize interfaces and internal variables for bmp085 
-	call bmp_085_init
-	
-	;call watchdog_init_interrupt_mode
-	
-	sei							; enable global interupts and reset TWCR registe
-
-	
-	call bmp_085_read_calibrations	
-	
-_sleep_loop:	
-	lds r16, SREG
-	sbrs r16, 0x07		; skip next instruction if global interupt enable bit is set
-	sei
-	sleep
-	call bmp_085_reset_actions_and_states
-	rjmp _sleep_loop
-	ret
-	
-; send twi status to usrt
-send_to_usart:
-	push r24
-	push r25
-	call hex2str
-	call usart_transmit_data
-	mov r24, r25
-	call usart_transmit_data
-	pop r25
-	pop r24
-	ret
-	
-return_from_interrupt:
-	reti
 
 .end
